3b6dc154ffa51d1903ba3449cef8ac04f920bec3
dhuziran <danielhuziran@gmail.com>
2023-05-04 13:13:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// assigning value to the first variable of the code
+let num1 = 3 + 10 - 3
+// assigning value to the second variable of the code
+let num2 = 5 * 8 
+// assigning value to the third variable of the code
+let num3 = 6 * 6 + 3
+// creating the pop up alert that displays the text and the code
+alert(`You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`)
